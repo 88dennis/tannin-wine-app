03bcab4c26e25a7707cf617963ff06ef650f0628
Dennis <dennissarmiento808@gmail.com>
2019-06-08 14:10:42 -0700

    up

---
+++ client/build/precache-manifest.62caacc770d46026313a69a564a49fed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41404202778f0d50bb9d67eb84d4702a",
+    "url": "/tannin-wine-app/index.html"
+  },
+  {
+    "revision": "9781bafcf8320aa7c467",
+    "url": "/tannin-wine-app/static/css/main.75381a87.chunk.css"
+  },
+  {
+    "revision": "632664a759464b8bf061",
+    "url": "/tannin-wine-app/static/js/2.68f48b5c.chunk.js"
+  },
+  {
+    "revision": "9781bafcf8320aa7c467",
+    "url": "/tannin-wine-app/static/js/main.b0d4cb54.chunk.js"
+  },
+  {
+    "revision": "e2ba333974ebfec6d297",
+    "url": "/tannin-wine-app/static/js/runtime~main.51d1447e.js"
+  }
+]);
