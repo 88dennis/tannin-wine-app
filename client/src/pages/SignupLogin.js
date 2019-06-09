f03be578a12033c6547cc5645752cd448afb124a
Dennis <dennissarmiento808@gmail.com>
2019-06-08 18:11:40 -0700

    up

--- client/src/pages/SignupLogin.js
+++ client/src/pages/SignupLogin.js
@@ -39,7 +39,7 @@ class SignupLogin extends Component {
 
   handleSignupFormSubmit = async event => {
     event.preventDefault();
-    const { restaurant, firstName, lastName, email, password } = this.setState;
+    const { restaurant, firstName, lastName, email, password } = this.state;
     if (firstName && lastName && restaurant && email && password) {
       const userInfo = { firstName, lastName, restaurant, email, password }
       const loginInfor = { email, password }
@@ -92,7 +92,7 @@ class SignupLogin extends Component {
 
   handleLoginFormSubmit = event => {
     event.preventDefault();
-    const loginInfor = { email: this.state.loginemail, password: this.state.loginpassword}
+    const loginInfor = { email: this.state.loginemail, password: this.state.loginpassword }
     API.logIn(loginInfor).then(response => {
       console.log("USER OBJ: ", response);
       if (response.status === 200) {
@@ -133,7 +133,7 @@ class SignupLogin extends Component {
     }
     return (
       <Container>
-                <div className="tannintextwrap2">Tann</div>
+                <div className="tannintextwrap2">Tannin</div>
         <SignupLoginForm
           handleSubmitInputChange={this.handleSubmitInputChange}
           handleSignupFormSubmit={this.handleSignupFormSubmit}
